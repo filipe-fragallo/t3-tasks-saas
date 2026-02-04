9d98b40a7d18c0a4dc9e2357ca33d76808c37a2d
Filipe Fragallo <filipe.fragallo@lab2dev.com>
2026-02-04 17:19:39 -0300

    12345

--- src/server/better-auth/config.ts
+++ src/server/better-auth/config.ts
@@ -1,23 +1,37 @@
-import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
-
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { env } from "~/env";
 import { db } from "~/server/db";
+import { betterAuth } from "better-auth/minimal";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { user, account, session, verification } from "~/server/db/schema";
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db, {
-    provider: "pg",
-  }),
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+
   emailAndPassword: {
     enabled: true,
   },
-  socialProviders: {
-    github: {
-      clientId: env.BETTER_AUTH_GITHUB_CLIENT_ID,
-      clientSecret: env.BETTER_AUTH_GITHUB_CLIENT_SECRET,
-      redirectURI: "http://localhost:3000/api/auth/callback/github",
+
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: {
+      user,
+      account,
+      session,
+      verification,
     },
-  },
+  }),
+
+  socialProviders:
+    env.BETTER_AUTH_GITHUB_CLIENT_ID && env.BETTER_AUTH_GITHUB_CLIENT_SECRET
+      ? {
+          github: {
+            clientId: env.BETTER_AUTH_GITHUB_CLIENT_ID,
+            clientSecret: env.BETTER_AUTH_GITHUB_CLIENT_SECRET,
+          },
+        }
+      : undefined,
 });
 
 export type Session = typeof auth.$Infer.Session;
